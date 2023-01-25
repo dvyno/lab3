a88f450f27c3e799c7b9dd807cd2d0af26a6b580
dvyno <ced.david0704@gmail.com>
2023-01-25 13:45:38 -0800

    Add tests and fix reverse methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,32 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace2() {
+    int[] input = {3, 9, 10, 12, 6, 5};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{5, 6, 12, 10, 9, 3}, input);
+  }
+
+  @Test
+  public void testReverseInPlace3() {
+    int[] input = {3, 9, 10, 12, 6};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{6, 12, 10, 9, 3}, input);
+  }
+
+
+  @Test
+  public void testReversed2() {
+    int[] input = {3, 9, 10, 12, 6, 5};
+
+    assertArrayEquals(new int[]{5, 6, 12, 10, 9, 3}, ArrayExamples.reversed(input));
+  }
+
+  @Test
+  public void testReversed3() {
+    int[] input = {3, 9, 10, 12, 6};
+    assertArrayEquals(new int[]{6, 12, 10, 9, 3}, ArrayExamples.reversed(input));
+  }
 }
